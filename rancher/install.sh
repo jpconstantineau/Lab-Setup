6fe49d9faa7935a528e2a78bf1fb33e832b7d515
Pierre <jpconstantineau@gmail.com>
2020-09-26 15:43:16 -0600

    adding heimdall deployment file

--- rancher/install.sh
+++ rancher/install.sh
@@ -1,19 +1,40 @@
-helm repo add rancher-stable https://releases.rancher.com/server-charts/stable
-helm repo add jetstack https://charts.jetstack.io
-helm repo update
-
-kubectl create namespace cattle-system
-kubectl create namespace cert-manager
-
 
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 kubectl get pods --all-namespaces
 helm ls --all-namespaces
 
+# Install the CustomResourceDefinition resources separately
+kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.15.0/cert-manager.crds.yaml
+
+# **Important:**
+# If you are running Kubernetes v1.15 or below, you
+# will need to add the `--validate=false` flag to your
+# kubectl apply command, or else you will receive a
+# validation error relating to the
+# x-kubernetes-preserve-unknown-fields field in
+# cert-manager’s CustomResourceDefinition resources.
+# This is a benign error and occurs due to the way kubectl
+# performs resource validation.
+
+# Create the namespace for cert-manager
+kubectl create namespace cert-manager
+
+# Add the Jetstack Helm repository
+helm repo add jetstack https://charts.jetstack.io
+
+# Update your local Helm chart repository cache
+helm repo update
+
+# Install the cert-manager Helm chart
 helm install \
   cert-manager jetstack/cert-manager \
   --namespace cert-manager \
   --version v0.15.0
+
+helm repo add rancher-stable https://releases.rancher.com/server-charts/stable
+helm repo update
+kubectl create namespace cattle-system
+
 
 helm install rancher rancher-stable/rancher \
   --namespace cattle-system \
