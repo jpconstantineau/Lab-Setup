69499fc437049ec51fffe44ba665d56f574db803
Pierre <jpconstantineau@gmail.com>
2020-09-25 22:03:33 -0600

    moving rancher to stable

--- rancher/install.sh
+++ rancher/install.sh
@@ -1,8 +1,12 @@
-helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
+helm repo add rancher-stable https://releases.rancher.com/server-charts/stable
 
 kubectl create namespace cattle-system
 
-helm install rancher rancher-latest/rancher \
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+kubectl get pods --all-namespaces
+helm ls --all-namespaces
+
+helm install rancher rancher-stable/rancher \
   --namespace cattle-system \
   --set hostname=rancher.jpconstantineau.com
 
