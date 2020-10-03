5179467f5c8d90aac6763f8ac60a2ba207ad95df
Pierre <jpconstantineau@gmail.com>
2020-10-03 10:53:17 -0600

    updating script

--- cluster/setup.md
+++ cluster/setup.md
@@ -1,7 +1,7 @@
 # Setup of a freshly installed Ubuntu 20.0 Server on XPC-NG
 
-1 - Install XCP-NG VM tools
-2 - Run this script
+1. Install XCP-NG VM tools
+2. Run this script
 
 ```
 sudo curl -sfL https://raw.githubusercontent.com/jpconstantineau/Lab-Setup/master/cluster/setup.sh | sh -
--- cluster/setup.sh
+++ cluster/setup.sh
@@ -4,9 +4,9 @@ sudo apt upgrade -y
 sudo apt install nfs-common git -y
 sudo apt-get autoremove && apt-get autoclean
 
-sudo echo 'precedence ::ffff:0:0/96  100' >> /etc/gai.conf
+sudo sh -c 'echo "precedence ::ffff:0:0/96  100" >> /etc/gai.conf'
 
-curl -sfL https://get.k3s.io | sh -
+curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" sh -
 k3s kubectl get node
 
 sudo mount /dev/cdrom /mnt
