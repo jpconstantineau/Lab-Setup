c5f6bc21431d0be3605c358885ef847a47b22424
Pierre <jpconstantineau@gmail.com>
2020-10-03 10:37:00 -0600

    adding script to setup servers

---
+++ cluster/setup.md
@@ -0,0 +1,8 @@
+# Setup of a freshly installed Ubuntu 20.0 Server on XPC-NG
+
+1 - Install XCP-NG VM tools
+2 - Run this script
+
+```
+curl -sfL https://get.k3s.io | sh -
+```
---
+++ cluster/setup.sh
@@ -0,0 +1,13 @@
+sudo apt update
+sudo apt upgrade -y
+sudo apt install nfs-common git -y
+sudo apt-get autoremove && apt-get autoclean
+
+echo 'precedence ::ffff:0:0/96  100' >> /etc/gai.conf
+
+curl -sfL https://get.k3s.io | sh -
+k3s kubectl get node
+
+sudo mount /dev/cdrom /mnt
+sudo bash /mnt/Linux/install.sh
+sudo umount /dev/cdrom
