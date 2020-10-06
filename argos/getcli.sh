1779d327db96b66d312bdd605a8beec2a7cc6e82
Pierre <jpconstantineau@gmail.com>
2020-10-05 21:02:40 -0600

    adding argoscd cli

---
+++ argos/getcli.sh
@@ -0,0 +1,3 @@
+VERSION=$(curl --silent "https://api.github.com/repos/argoproj/argo-cd/releases/latest" | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/')
+curl -sSL -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/download/$VERSION/argocd-linux-amd64
+chmod +x /usr/local/bin/argocd
