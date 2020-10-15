bf77c67105e4e0aa2a34b00c4a1620acf8a781b0
Pierre <jpconstantineau@gmail.com>
2020-10-14 22:36:41 -0600

    removing ubuntu cloud

--- cluster/k3s-a.sh
+++ cluster/k3s-a.sh
@@ -5,6 +5,7 @@ k3s kubectl get all --all-namespaces
 
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/jpconstantineau/Lab-Setup/master/argos/install.yaml
+kubectl apply -n argocd -f https://raw.githubusercontent.com/jpconstantineau/Lab-Setup/master/argos/service.yaml
 
 VERSION=$(curl --silent "https://api.github.com/repos/argoproj/argo-cd/releases/latest" | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/')
 sudo curl -SL -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/download/$VERSION/argocd-linux-amd64
@@ -13,4 +14,4 @@ sudo chmod +x /usr/local/bin/argocd
 
 kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o name | cut -d'/' -f 2
 
-argocd login localhost
+argocd login k3s-a
--- cluster/setup.sh
+++ cluster/setup.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+sudo touch /etc/cloud/cloud-init.disabled
+
+dpkg-reconfigure cloud-init
+
+sudo dpkg-reconfigure cloud-init
+sudo apt-get purge cloud-init
+sudo rm -rf /etc/cloud/ && sudo rm -rf /var/lib/cloud/
+
 sudo apt update
 sudo apt upgrade -y
 sudo apt install nfs-common git -y
