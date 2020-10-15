38a2e0b17a12e81962c21a964a6c2154da2d8b72
Pierre <jpconstantineau@gmail.com>
2020-10-14 21:24:46 -0600

    updating k3s-a to argocd

--- cluster/get-flux.sh
+++ cluster/get-flux.sh
@@ -1,3 +1,3 @@
-VERSION=$(curl --silent "https://github.com/fluxcd/flux/releases/latest" | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/')
+VERSION=$(curl --silent "https://api.github.com/repos/fluxcd/flux/releases/latest" | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/')
 curl -sSL -o /usr/local/bin/fluxctl https://github.com/fluxcd/flux/releases/download/$VERSION/fluxctl_linux_amd64
 chmod +x /usr/local/bin/fluxctl
--- cluster/k3s-a.sh
+++ cluster/k3s-a.sh
@@ -9,16 +9,19 @@ sudo sh -c 'echo "precedence ::ffff:0:0/96  100" >> /etc/gai.conf'
 curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="server -t agent-secret-token" sh -s -
 k3s kubectl get node
 
-kubectl create ns flux
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-export GHUSER="jpconstantineau"
-fluxctl install \
---git-user=${GHUSER} \
---git-email=${GHUSER}@users.noreply.github.com \
---git-url=git@github.com:${GHUSER}/Lab-Setup \
---git-path=deployments \
---namespace=flux | kubectl apply -f -
+VERSION=$(curl --silent "https://api.github.com/repos/argoproj/argo-cd/releases/latest" | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/')
+curl -sSL -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/download/$VERSION/argocd-linux-amd64
+chmod +x /usr/local/bin/argocd
+
+kubectl port-forward svc/argocd-server -n argocd 8080:443
 
 sudo mount /dev/cdrom /mnt
 sudo bash /mnt/Linux/install.sh
 sudo umount /dev/cdrom
+
+kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o name | cut -d'/' -f 2
+
+argocd login localhost
